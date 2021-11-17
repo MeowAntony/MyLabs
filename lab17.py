45795b83861c39ec0d95418815bf6e922c1cfe19
Антоша <anton13@myrambler.ru>
2021-11-17 11:32:21 +0300

    сделал 17 лабу

--- lab17.py
+++ lab17.py
@@ -1,10 +1,12 @@
+import tree_to_picture
+
+
 class Cell:
     def __init__(self, value=None, left=None, right=None, parent=None):
         self.value = value
         self.left: Cell = left
         self.right: Cell = right
         self.parent: Cell = parent
-
 
 
 def construction_tree(cell_now, tree_str: str) -> Cell:
@@ -18,10 +20,6 @@ def construction_tree(cell_now, tree_str: str) -> Cell:
         index_start = 0
         index_comma = tree_str.find(',', index_start)
         while tree_str.count('(', 0, index_comma) != tree_str.count(')', 0, index_comma):
-            # print("tree: ", tree_str)
-            # print(tree_str.count('(', 0, index_comma), tree_str.count(')', 0, index_comma))
-            # print(index_start, index_comma)
-            # print()
             index_start = index_comma + 1
             index_comma = tree_str.find(',', index_start)
         left_tree = tree_str[:index_comma]
@@ -33,22 +31,95 @@ def construction_tree(cell_now, tree_str: str) -> Cell:
             cell_now.right = Cell(parent=cell_now)
             construction_tree(cell_now.right, right_tree)
 
-def search_value_tree(cell_tree: Cell, value):
-    if cell_tree is not None and
+
+def search_value_tree(cell_tree: Cell, value: int):
+    if cell_tree is None or cell_tree.value == value:
+        return cell_tree
+    if value < cell_tree.value:
+        return search_value_tree(cell_tree.left, value)
+    else:
+        return search_value_tree(cell_tree.right, value)
+
+
+def add_value_tree(cell_tree: Cell, cell: Cell):
+    if cell_tree.value > cell.value and not cell_tree.left:
+        cell.parent = cell_tree
+        cell_tree.left = cell
+    elif cell_tree.value < cell.value and not cell_tree.right:
+        cell.parent = cell_tree
+        cell_tree.right = cell
+    elif cell_tree.left and cell_tree.value > cell.value:
+        add_value_tree(cell_tree.left, cell)
+    elif cell_tree.right and cell_tree.value < cell.value:
+        add_value_tree(cell_tree.right, cell)
+
+
+def delete_value_tree(cell_tree: Cell, value):
+    if cell_tree is None:
+        return False
+    if cell_tree.value > value:
+        return delete_value_tree(cell_tree.left, value)
+    elif cell_tree.value < value:
+        return delete_value_tree(cell_tree.right, value)
+    else:
+        if cell_tree.left is None and cell_tree.right is None:
+            if cell_tree == cell_tree.parent.left:
+                cell_tree.parent.left = None
+            else:
+                cell_tree.parent.right = None
+            del cell_tree
+        elif cell_tree.left is None and cell_tree.right is not None:
+            cell_tree.value = cell_tree.right.value
+            cell_tree.left = cell_tree.right.left
+            cell_tree.right = cell_tree.right.right
+        elif cell_tree.left is not None and cell_tree.right is None:
+            cell_tree.value = cell_tree.left.value
+            cell_tree.right = cell_tree.left.right
+            cell_tree.left = cell_tree.left.left
+        else:
+            if cell_tree.right.left is None:
+                cell_tree.value = cell_tree.right.value
+                cell_tree.right = cell_tree.right.right
+            else:
+                min_left = cell_tree.right.left
+                while min_left.left is not None:
+                    min_left = min_left.left
+                cell_tree.value = min_left.value
+                min_left.parent.left = min_left.right
+        return True
+
+
 if __name__ == '__main__':  # 8 (3 (1, 6 (4,7)), 10 (, 14(13,)))
     tree = input("Введите дерево в виде линейно-скобочной записи: \n").replace(' ', '')
     cell_tree = Cell()
     construction_tree(cell_tree, tree)
     while True:
-        choose = input("""
-        Выберите действие:\n
-        1) Поиск элемента
-        2) Добавить элемент
-        3) Удалить элемент
-        4) Показать дерево (строчный вариант)
-        5) Показать дерево (картинка)
-        6) Выход
-        """)
-
+        choose_text = """Выберите действие:
+        1) Поиск элемента 
+        2) Добавить элемент 
+        3) Удалить элемент 
+        4) Показать дерево (картинка) 
+        5) Выход \n"""
+        choose = input(choose_text)
+        print()
         if choose == "1":
-
+            search_value = int(input("Введите значение элемента: "))
+            if search_value_tree(cell_tree, search_value) is not None:
+                print("Данный элемент присутствует в дереве")
+            else:
+                print("Данный элемент не найден")
+        elif choose == "2":
+            add_value = int(input("Введите значение элемента: "))
+            add_value_tree(cell_tree, Cell(add_value))
+        elif choose == "3":
+            delete_value = int(input("Введите значение элемента: "))
+            if delete_value_tree(cell_tree, delete_value):
+                print("Успешно удалён элемент")
+            else:
+                print("Данный элемент отсутствует")
+        elif choose == "4":
+            tree_to_picture.main(cell_tree)
+        elif choose == "5":
+            exit(1)
+        else:
+            continue
---
+++ tree_to_picture.py
@@ -0,0 +1,114 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
+class Cell:
+    def __init__(self, value, left, right, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+
+def generate_tree(mas, index, cell_now):
+    cell_left = Cell(mas[index * 2 + 1], None, None, parent=cell_now) if len(mas) > index * 2 + 1 else None
+    cell_right = Cell(mas[index * 2 + 2], None, None, parent=cell_now) if len(mas) > index * 2 + 2 else None
+    cell_now.left = cell_left
+    cell_now.right = cell_right
+    if cell_left:
+        generate_tree(mas, index * 2 + 1, cell_left)
+    if cell_right:
+        generate_tree(mas, index * 2 + 2, cell_right)
+
+
+def get_glubina(cell_now: Cell):
+    if cell_now is None:
+        return 0
+    return 1 + max(get_glubina(cell_now.left), get_glubina(cell_now.right))
+
+
+def get_x_y(diameter, level_now, level_max, index_element):
+    """
+    Получаем x и y для клеточки
+
+    :param diameter: Дисаметр
+    :param level_now: какой сейчас уровень (сверху вниз)
+    :param level_max: максимальный уровень
+    :param index_element: индекс элемента на строке
+    :return: x и y
+    """
+    for_x = 0
+    for i in range(level_max - level_now):
+        for_x = for_x * 2 + 1
+    x_plus = (diameter * 3 / 4) * for_x
+    x = 100 + x_plus + index_element * ((diameter * 3 // 2) * (2 ** (level_max - level_now)))
+    y = 50 + (diameter * 3 / 2) * level_now
+    return x, y
+
+
+def draw_pole(draw, diameter, level_now, level_max, index_element, cell_now: Cell):
+    """
+    Рисуем наше деревко
+
+    :param draw: Рисовалка
+    :param diameter: Дисаметр
+    :param level_now: какой сейчас уровень (сверху вниз)
+    :param level_max: максимальный уровень
+    :param index_element: индекс элемента на строке
+    :param cell_now: текущая клеточка
+    """
+
+    x, y = get_x_y(diameter, level_now, level_max, index_element)
+
+    if cell_now.left is not None:
+        x_left, y_left = get_x_y(diameter, level_now + 1, level_max, index_element * 2)
+
+        draw.line((x + diameter // 2, y + diameter // 2,
+                   x_left + diameter // 2, y_left + diameter // 2), fill=(255, 255, 255),
+                  width=diameter // 20)
+
+        draw_pole(draw, diameter, level_now + 1, level_max, index_element * 2, cell_now.left)
+
+    if cell_now.right is not None:
+        x_right, y_right = get_x_y(diameter, level_now + 1, level_max, index_element * 2 + 1)
+
+        draw.line((x + diameter // 2, y + diameter // 2,
+                   x_right + diameter // 2, y_right + diameter // 2), fill=(255, 255, 255),
+                  width=diameter // 20)
+
+        draw_pole(draw, diameter, level_now + 1, level_max, index_element * 2 + 1, cell_now.right)
+
+    draw.ellipse((x, y, x + diameter, y + diameter), fill=(255, 255, 255))
+    font = ImageFont.truetype("./123.ttf",
+                              diameter // 2)  # FIXME вот тут шрифт задавать, можно контролировать в зависимости от числа
+    draw.text((x + diameter // 2, y + diameter // 2),
+              str(cell_now.value) if cell_now.value else "", anchor="mm", font=font, fill=(0, 0, 0))
+
+
+def main(cell: Cell):
+    glubina = get_glubina(cell) - 1
+    diam = 100
+    img = Image.new("RGBA", (200 + diam + ((2 ** glubina) - 1) * (diam * 3 // 2),
+                             100 + diam + (diam * 3 // 2) * glubina), 'grey')
+    draw_img = ImageDraw.Draw(img)
+
+    draw_pole(draw_img, diam, 0, glubina, 0, cell)
+
+    img.show()
+# for i in range(4):
+#     draw_img.ellipse((100 + i * (d * 1.5), 100 + (d * 3 / 2) * 2,
+#                       100 + d + i * (d * 1.5), 100 + (d * 3 / 2) * 2 + d))
+#
+# for i in range(2):#75 1
+#     draw_img.ellipse((100 + d * 3 // 4 + i * (d * 1.5) * 2, 100 + (d * 3 / 2) * 1,
+#                       100 + d + d * 3 // 4 + i * (d * 1.5) * 2, 100 + (d * 3 / 2) * 1 + d))
+#     pass
+# for i in range(1):#150 3
+#     draw_img.ellipse((100 + (d * 3 // 4) * 3 + i * (d * 1.5) * 2, 100,
+#                       100 + d + (d * 3 // 4) * 3 + i * (d * 1.5) * 2, 100 + d))
+#     pass
+# for i in range(2):#300 7
+#     draw.ellipse((100 + 525 + i * 1200, 250, 725 + i * 1200, 350))
+#     pass
+# for i in range(1):#600 15
+#     draw.ellipse((100 + 1125 + i * 2400, 100, 1350 + i * 2400, 200))
+#     pass
